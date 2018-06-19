5864f1c1868809d45f9bddbb9d7fce1ca1d81b13
Hoyt Chang <hoyt.chang@gmail.com>
2018-06-18 20:25:37 -0400

    learning curve
    added sklearn learning curve

--- classify.py
+++ classify.py
@@ -5,6 +5,8 @@ from sklearn import preprocessing
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
 from sklearn import svm
+import matplotlib.pyplot as plt
+from sklearn.model_selection import learning_curve
 
 # Read in financial data
 data = pd.read_csv('SP500_financial_data.csv', index_col = False)
@@ -75,3 +77,56 @@ for i in range(10):
 
 print('Accuracy of K = ' + str(n_neighbors) + ' nearest neighbors, average of 10 cross validations: ' + str(np.mean(accuracy_array_knn))[0:5])
 print('Accuracy of SVM with ' + kernel + ' kernel, average of 10 cross validations: ' + str(np.mean(accuracy_array_svm))[0:5])
+
+# Plot learning curve
+def plot_learning_curve(estimator, title, X, y, ylim = None, cv = None, n_jobs = 1, train_sizes = np.linspace(0.1, 1.0, 10)):
+	plt.figure()
+	plt.title(title)
+	if ylim is not None:
+		plt.ylim(*ylim)
+	plt.xlabel("Training examples")
+	plt.ylabel("Score")
+	train_sizes, train_scores, test_scores = learning_curve(estimator, X, y, cv = cv, n_jobs = n_jobs, train_sizes = train_sizes)
+	train_scores_mean = np.mean(train_scores, axis = 1)
+	train_scores_std = np.std(train_scores, axis = 1)
+	test_scores_mean = np.mean(test_scores, axis = 1)
+	test_scores_std = np.std(test_scores, axis = 1)
+	plt.grid()
+	plt.fill_between(train_sizes, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, alpha = 0.1, color = "r")
+	plt.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha = 0.1, color = 'g')
+	plt.plot(train_sizes, train_scores_mean, 'o-', color = 'r', label = 'Training score')
+	plt.plot(train_sizes, test_scores_mean, 'o-', color = 'g', label = 'Validation score')
+	plt.legend(loc = 'best')
+	#return plt
+
+# Plot validation curve
+def plot_validation_curve(estimator, title, X, y, param_name, param_range, ylim = None, cv = None, n_jobs = 1, train_size = np.linspace(0.1, 1.0, 5)):
+	plt.figure()
+	train_scores, test_scores = validation_curve(estimator, X, y, param_name, param_range, cv)
+	train_mean = np.mean(train_scores, axis = 1)
+	train_std = np.std(train_scores, axis = 1)
+	test_mean = np.mean(test_scores, axis = 1)
+	test_std = np.std(test_scores, axis = 1)
+	plt.plot(param_range, train_mean, color = 'r', marker = 'o', markersize = 5, label = 'Training score')
+	plt.fill_between(param_range, train_mean + train_std, train_mean - train_std, alpha = 0.15, color = 'r')
+	plt.plot(param_range, test_mean, color = 'g', linestyle = '--', marker = 's', markersize = 5, label = 'Validation score')
+	plt.fill_between(param_range, test_mean + test_std, test_mean - test_std, alpha = 0.15, color = 'g')
+	plt.grid()
+	plt.xscale('log')
+	plt.legend(loc = 'best')
+	plt.xlabel('Parameter')
+	plt.ylabel('Score')
+	plt.ylim(ylim)
+
+# Create K nearest neighbor model using full data set
+X_train = data[['ROE','ROA','PB_ratio','Asset_turn',\
+		'LT_debt_to_equity','Equity_to_assets','Current_ratio','Div_yield','Capex_to_revenue']]
+y_train = data['Sector Encoded']
+n_neighbors = 3
+neigh = KNeighborsClassifier(n_neighbors = n_neighbors)
+neigh.fit(X_train, y_train)
+
+# Plot learning curves
+title = 'Learning Curves (KNN)'
+plot_learning_curve(neigh, title, X_train, y_train, ylim=(0.1, 1.01), cv=10, n_jobs=1)
+plt.show()
